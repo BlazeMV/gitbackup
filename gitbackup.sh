27a62629336e1146720fa6505f18af5272375455
= <ad.adamdavid72@gmail.com>
2018-10-27 17:04:54 +0500

    changed to a bash script + fetch config from config file + fetch org repos and private repos

--- gitbackup.sh
+++ gitbackup.sh
@@ -1,7 +1,6 @@
-#!/bin/sh
-github_username="ameer1234567890"
-url_start="https://github.com/"
+#!/bin/bash
 
+#check if required tools is available
 check_tools() {
   tools="curl git"
   for tool in $tools; do
@@ -11,14 +10,39 @@ check_tools() {
     fi
   done
 }
-
 check_tools
 
+#get config values from config file
+source config.cfg
+
+#set case-insensitive matches
+shopt -s nocasematch
+
 echo "Grabbing list of repositories...."
-curl --progress-bar -o repos.json https://api.github.com/users/$github_username/repos
+
+if [[ $FETCH_PRIVATE =~ ^true$ ]] ; then
+
+    url_start="https://$GITHUB_TOKEN@github.com/"
+
+    if [[ $FETCH_ORGANIZATION =~ ^true$ ]] ; then
+        curl --progress-bar -o repos.json -u $GITHUB_USERNAME:$GITHUB_TOKEN https://api.github.com/orgs/$GITHUB_ORGANIZATION/repos
+    else
+        curl --progress-bar -o repos.json -u $GITHUB_USERNAME:$GITHUB_TOKEN https://api.github.com/users/$GITHUB_USERNAME/repos
+    fi
+else
+    url_start="https://github.com/"
+    if [[ $FETCH_ORGANIZATION =~ ^true$ ]] ; then
+        curl --progress-bar -o repos.json https://api.github.com/orgs/$GITHUB_ORGANIZATION/repos
+    else
+        curl --progress-bar -o repos.json https://api.github.com/users/$GITHUB_USERNAME/repos
+    fi
+fi
+
 repos="$(grep -Po '"full_name":.*?[^\\]",' repos.json | awk '{print $2}' | tr -d '"' | tr -d ',')"
+
 mkdir -p repos
 cd repos || exit
+
 for repo in $repos; do
   repo_name="$(echo "$repo" | cut -d '/' -f 2)"
   if [ ! -d "$repo_name" ]; then
@@ -40,7 +64,6 @@ for repo in $repos; do
   echo ""
 done
 cd ..
-rm repos.json
 echo "All repositories backed up!"
 
 printf "Press enter to exit..."
